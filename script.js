ab3b5c94ca67366622a87f1c43070d304c8676aa
Sander Meijer <sandertje1972@hotmail.com>
2021-03-12 09:53:22 +0100

    script.js toegevoegd

---
+++ script.js
@@ -0,0 +1,44 @@
+// A
+// zet de functie om naar een arrow functie
+const ikRockArrowFunctions = function () {
+    console.log("Joe, ik rock de arrow functions!");
+  };
+
+const ikRockArrowFunctions2 = () => console.log("Yeah, ik rock de arrow functions!");
+
+ console.log("A"); 
+ikRockArrowFunctions();
+ikRockArrowFunctions2();
+
+// B
+const fivePlusSeven = function () {
+  return 5 + 7
+};
+
+const fivePlusSeven2 = () => 5 + 7;
+
+fivePlusSeven();
+fivePlusSeven2();
+console.log("B");
+console.log(fivePlusSeven());
+console.log(fivePlusSeven2());
+
+// C
+const myFunction = (a,b) => a + b;
+
+console.log("C");
+console.log(myFunction(2,3));
+console.log(myFunction(5,8));
+
+// D
+const addFive = (a) => a + 5;
+
+console.log("D");
+console.log(addFive(3));
+console.log(addFive(93));
+
+// E
+const createObject = () => ({greeting : "Hoi"});
+
+
+
